5eb8a1f8973825d8682169f72a25d9de23004290
YimingZhanshen <76594627+YimingZhanshen@users.noreply.github.com>
2026-02-02 00:48:19 +0800

    Delete Qt directory

--- Qt/5.15.2/msvc2019/bin/qtenv2.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Setting up environment for Qt usage...
-set PATH=C:\Qt\5.15.2\msvc2019\bin;%PATH%
-cd /D C:\Qt\5.15.2\msvc2019
-echo Remember to call vcvarsall.bat to complete environment setup!
